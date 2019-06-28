3a7c730cf8d422bf7027fd912c45866b2665272e
Prospector <prospectordev@gmail.com>
2019-06-27 22:00:12 -0700

    Try to clean up the names a tiny bit + fix swamp tree using wrong blocks

--- src/main/java/io/github/terraformersmc/terraform/feature/TerraformSwampTreeFeature.java
+++ src/main/java/io/github/terraformersmc/terraform/feature/TerraformSwampTreeFeature.java
@@ -44,57 +44,57 @@ public class TerraformSwampTreeFeature extends AbstractTreeFeature<DefaultFeatur
     public boolean generate(Set<BlockPos> blocks, ModifiableTestableWorld world, Random random, BlockPos pos, MutableIntBoundingBox boundingBox) {
         int height = random.nextInt(4) + minHeight;
         pos = world.getTopPosition(Heightmap.Type.OCEAN_FLOOR, pos);
-        boolean var6 = true;
+        boolean generate = true;
         if (pos.getY() >= 1 && pos.getY() + height + 1 <= 256) {
-            int var7;
+            int y;
             int var10;
             int var11;
-            for (var7 = pos.getY(); var7 <= pos.getY() + 1 + height; ++var7) {
-                int var8 = 1;
-                if (var7 == pos.getY()) {
-                    var8 = 0;
+            for (y = pos.getY(); y <= pos.getY() + 1 + height; ++y) {
+                int offset = 1;
+                if (y == pos.getY()) {
+                    offset = 0;
                 }
 
-                if (var7 >= pos.getY() + 1 + height - 2) {
-                    var8 = 3;
+                if (y >= pos.getY() + 1 + height - 2) {
+                    offset = 3;
                 }
 
-                BlockPos.Mutable var9 = new BlockPos.Mutable();
+                BlockPos.Mutable checking = new BlockPos.Mutable();
 
-                for (var10 = pos.getX() - var8; var10 <= pos.getX() + var8 && var6; ++var10) {
-                    for (var11 = pos.getZ() - var8; var11 <= pos.getZ() + var8 && var6; ++var11) {
-                        if (var7 >= 0 && var7 < 256) {
-                            var9.set(var10, var7, var11);
-                            if (!isAirOrLeaves(world, var9)) {
-                                if (isWater(world, var9)) {
-                                    if (var7 > pos.getY()) {
-                                        var6 = false;
+                for (var10 = pos.getX() - offset; var10 <= pos.getX() + offset && generate; ++var10) {
+                    for (var11 = pos.getZ() - offset; var11 <= pos.getZ() + offset && generate; ++var11) {
+                        if (y >= 0 && y < 256) {
+                            checking.set(var10, y, var11);
+                            if (!isAirOrLeaves(world, checking)) {
+                                if (isWater(world, checking)) {
+                                    if (y > pos.getY()) {
+                                        generate = false;
                                     }
                                 }
                                 else {
-                                    var6 = false;
+                                    generate = false;
                                 }
                             }
                         }
                         else {
-                            var6 = false;
+                            generate = false;
                         }
                     }
                 }
             }
 
-            if (!var6) {
+            if (!generate) {
                 return false;
             }
             else if (isNaturalDirtOrGrass(world, pos.down()) && pos.getY() < 256 - height - 1) {
                 this.setToDirt(world, pos.down());
 
                 int var12;
-                BlockPos var14;
+                BlockPos leavesPos;
                 int var17;
                 int var18;
-                for (var7 = pos.getY() - 3 + height; var7 <= pos.getY() + height; ++var7) {
-                    var17 = var7 - (pos.getY() + height);
+                for (y = pos.getY() - 3 + height; y <= pos.getY() + height; ++y) {
+                    var17 = y - (pos.getY() + height);
                     var18 = 2 - var17 / 2;
 
                     for (var10 = pos.getX() - var18; var10 <= pos.getX() + var18; ++var10) {
@@ -103,49 +103,49 @@ public class TerraformSwampTreeFeature extends AbstractTreeFeature<DefaultFeatur
                         for (var12 = pos.getZ() - var18; var12 <= pos.getZ() + var18; ++var12) {
                             int var13 = var12 - pos.getZ();
                             if (Math.abs(var11) != var18 || Math.abs(var13) != var18 || random.nextInt(2) != 0 && var17 != 0) {
-                                var14 = new BlockPos(var10, var7, var12);
-                                if (isAirOrLeaves(world, var14) || isReplaceablePlant(world, var14)) {
-                                    this.setBlockState(world, var14, OAK_LEAVES);
+                                leavesPos = new BlockPos(var10, y, var12);
+                                if (isAirOrLeaves(world, leavesPos) || isReplaceablePlant(world, leavesPos)) {
+                                    this.setBlockState(world, leavesPos, leaves);
                                 }
                             }
                         }
                     }
                 }
 
-                for (var7 = 0; var7 < height; ++var7) {
-                    BlockPos var19 = pos.up(var7);
-                    if (isAirOrLeaves(world, var19) || isWater(world, var19)) {
-                        this.setBlockState(blocks, world, var19, OAK_LOG, boundingBox);
+                for (y = 0; y < height; ++y) {
+                    BlockPos logPos = pos.up(y);
+                    if (isAirOrLeaves(world, logPos) || isWater(world, logPos)) {
+                        this.setBlockState(blocks, world, logPos, log, boundingBox);
                     }
                 }
 
-                for (var7 = pos.getY() - 3 + height; var7 <= pos.getY() + height; ++var7) {
-                    var17 = var7 - (pos.getY() + height);
+                for (y = pos.getY() - 3 + height; y <= pos.getY() + height; ++y) {
+                    var17 = y - (pos.getY() + height);
                     var18 = 2 - var17 / 2;
-                    BlockPos.Mutable var20 = new BlockPos.Mutable();
+                    BlockPos.Mutable outerLeavesPos = new BlockPos.Mutable();
 
                     for (var11 = pos.getX() - var18; var11 <= pos.getX() + var18; ++var11) {
                         for (var12 = pos.getZ() - var18; var12 <= pos.getZ() + var18; ++var12) {
-                            var20.set(var11, var7, var12);
-                            if (isLeaves(world, var20)) {
-                                BlockPos var21 = var20.west();
-                                var14 = var20.east();
-                                BlockPos var15 = var20.north();
-                                BlockPos var16 = var20.south();
-                                if (random.nextInt(4) == 0 && isAir(world, var21)) {
-                                    this.addVines(world, var21, VineBlock.EAST);
+                            outerLeavesPos.set(var11, y, var12);
+                            if (isLeaves(world, outerLeavesPos)) {
+                                BlockPos westPos = outerLeavesPos.west();
+                                leavesPos = outerLeavesPos.east();
+                                BlockPos norhtPos = outerLeavesPos.north();
+                                BlockPos southPos = outerLeavesPos.south();
+                                if (random.nextInt(4) == 0 && isAir(world, westPos)) {
+                                    this.addVines(world, westPos, VineBlock.EAST);
                                 }
 
-                                if (random.nextInt(4) == 0 && isAir(world, var14)) {
-                                    this.addVines(world, var14, VineBlock.WEST);
+                                if (random.nextInt(4) == 0 && isAir(world, leavesPos)) {
+                                    this.addVines(world, leavesPos, VineBlock.WEST);
                                 }
 
-                                if (random.nextInt(4) == 0 && isAir(world, var15)) {
-                                    this.addVines(world, var15, VineBlock.SOUTH);
+                                if (random.nextInt(4) == 0 && isAir(world, norhtPos)) {
+                                    this.addVines(world, norhtPos, VineBlock.SOUTH);
                                 }
 
-                                if (random.nextInt(4) == 0 && isAir(world, var16)) {
-                                    this.addVines(world, var16, VineBlock.NORTH);
+                                if (random.nextInt(4) == 0 && isAir(world, southPos)) {
+                                    this.addVines(world, southPos, VineBlock.NORTH);
                                 }
                             }
                         }
