78467f780a57ee34b1dad9047cf1aa8ee66ee2a1
Brandon Davis <neusfear@gmail.com>
2019-08-21 12:26:11 -0500

    Use sand tags instead of direct block comparisons for desert plants

--- src/main/java/com/terraformersmc/terraform/block/TerraformDesertPlantBlock.java
+++ src/main/java/com/terraformersmc/terraform/block/TerraformDesertPlantBlock.java
@@ -1,9 +1,8 @@
 package com.terraformersmc.terraform.block;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.PlantBlock;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 
@@ -14,8 +13,7 @@ public class TerraformDesertPlantBlock extends PlantBlock {
 
 	@Override
 	protected boolean canPlantOnTop(BlockState blockState_1, BlockView blockView_1, BlockPos blockPos_1) {
-		Block block_1 = blockState_1.getBlock();
-		if (block_1 == Blocks.SAND || block_1 == Blocks.RED_SAND) {
+		if (BlockTags.SAND.contains(blockState_1.getBlock())) {
 			return true;
 		}
 		return super.canPlantOnTop(blockState_1, blockView_1, blockPos_1);
