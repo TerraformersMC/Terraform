ddbe670093d32f03e3f4ece1f7b837e2e13321cd
Prospector <prospectordev@gmail.com>
2019-12-31 02:54:31 -0800

    v1.3.4 - Add hook for slime spawn biome

---
+++ src/main/java/com/terraformersmc/terraform/mixin/MixinSlimeEntity.java
@@ -0,0 +1,33 @@
+package com.terraformersmc.terraform.mixin;
+
+import com.terraformersmc.terraform.util.TerraformBiomeSets;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnType;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.Monster;
+import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(SlimeEntity.class)
+public abstract class MixinSlimeEntity extends MobEntity implements Monster {
+	public MixinSlimeEntity(EntityType<? extends MobEntity> type, World world) {
+		super(type, world);
+	}
+
+	@Inject(method = "canSpawn", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/IWorld;getBiome(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/world/biome/Biome;"), cancellable = true)
+	private static void canSpawnInjection(EntityType<SlimeEntity> type, IWorld world, SpawnType spawnType, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> info) {
+		Biome biome = world.getBiome(pos);
+		if (TerraformBiomeSets.getSlimeSpawnBiomes().contains(biome) && pos.getY() > 50 && pos.getY() < 70 && random.nextFloat() < 0.5F && random.nextFloat() < world.getMoonSize() && world.getLightLevel(pos) <= random.nextInt(8)) {
+			info.setReturnValue(canMobSpawn(type, world, spawnType, pos, random));
+		}
+	}
+}
---
+++ src/main/java/com/terraformersmc/terraform/util/TerraformBiomeSets.java
@@ -0,0 +1,23 @@
+package com.terraformersmc.terraform.util;
+
+import net.minecraft.world.biome.Biome;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TerraformBiomeSets {
+	private static final Set<Biome> SLIME_SPAWN_BIOMES = new HashSet<>();
+
+	public static Set<Biome> getSlimeSpawnBiomes() {
+		return SLIME_SPAWN_BIOMES;
+	}
+
+	public static void addSlimeSpawnBiome(Biome biome) {
+		SLIME_SPAWN_BIOMES.add(biome);
+	}
+
+	public static void addSlimeSpawnBiomes(Biome... biomes) {
+		SLIME_SPAWN_BIOMES.addAll(Arrays.asList(biomes));
+	}
+}
