1394b300b25bd3abf5167257404e3d2d48aaaf1b
Prospector <prospectordev@gmail.com>
2020-06-09 03:43:24 -0700

    Update yarn

--- src/main/java/com/terraformersmc/terraform/surface/CliffSurfaceConfig.java
+++ src/main/java/com/terraformersmc/terraform/surface/CliffSurfaceConfig.java
@@ -8,10 +8,10 @@ import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
 public class CliffSurfaceConfig extends TernarySurfaceConfig {
 	public static final Codec<CliffSurfaceConfig> CODEC = RecordCodecBuilder.create((instance) ->
 			instance.group(
-					BlockState.field_24734.fieldOf("top_material").forGetter(CliffSurfaceConfig::getTopMaterial),
-					BlockState.field_24734.fieldOf("under_material").forGetter(CliffSurfaceConfig::getUnderMaterial),
-					BlockState.field_24734.fieldOf("underwater_material").forGetter(CliffSurfaceConfig::getUnderwaterMaterial),
-					BlockState.field_24734.fieldOf("cliff_material").forGetter(CliffSurfaceConfig::getCliffMaterial)).apply(instance, CliffSurfaceConfig::new));
+					BlockState.CODEC.fieldOf("top_material").forGetter(CliffSurfaceConfig::getTopMaterial),
+					BlockState.CODEC.fieldOf("under_material").forGetter(CliffSurfaceConfig::getUnderMaterial),
+					BlockState.CODEC.fieldOf("underwater_material").forGetter(CliffSurfaceConfig::getUnderwaterMaterial),
+					BlockState.CODEC.fieldOf("cliff_material").forGetter(CliffSurfaceConfig::getCliffMaterial)).apply(instance, CliffSurfaceConfig::new));
 	private final BlockState cliffMaterial;
 
 	public CliffSurfaceConfig(BlockState top, BlockState under, BlockState underwater, BlockState cliff) {
