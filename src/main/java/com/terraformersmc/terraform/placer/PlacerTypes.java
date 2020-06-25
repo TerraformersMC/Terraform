b7a76a26eae5935b9adb454f166b26f5c3ce1ef7
Prospector <prospectordev@gmail.com>
2020-06-25 00:16:15 -0700

    2.1.0 - one more method here

--- src/main/java/com/terraformersmc/terraform/placer/PlacerTypes.java
+++ src/main/java/com/terraformersmc/terraform/placer/PlacerTypes.java
@@ -3,6 +3,7 @@ package com.terraformersmc.terraform.placer;
 import com.mojang.serialization.Codec;
 import com.terraformersmc.terraform.mixin.InvokerFoliagePlacerType;
 import com.terraformersmc.terraform.mixin.InvokerTrunkPlacerType;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.foliage.FoliagePlacer;
 import net.minecraft.world.gen.foliage.FoliagePlacerType;
 import net.minecraft.world.gen.trunk.TrunkPlacer;
@@ -15,5 +16,13 @@ public class PlacerTypes {
 
 	public static <P extends TrunkPlacer> TrunkPlacerType<P> registerTrunkPlacer(String id, Codec<P> codec) {
 		return InvokerTrunkPlacerType.callRegister(id, codec);
+	}
+
+	public static <P extends FoliagePlacer> FoliagePlacerType<P> registerFoliagePlacer(Identifier id, Codec<P> codec) {
+		return registerFoliagePlacer(id.toString(), codec);
+	}
+
+	public static <P extends TrunkPlacer> TrunkPlacerType<P> registerTrunkPlacer(Identifier id, Codec<P> codec) {
+		return registerTrunkPlacer(id.toString(), codec);
 	}
 }
