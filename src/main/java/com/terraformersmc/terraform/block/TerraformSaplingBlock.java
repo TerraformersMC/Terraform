ce985936b1f41f459f5b8c45327ca9fe0ee8ac2c
Prospector <prospectordev@gmail.com>
2020-06-28 01:35:49 -0700

    Add old constructor back to fix old mods

--- src/main/java/com/terraformersmc/terraform/block/TerraformSaplingBlock.java
+++ src/main/java/com/terraformersmc/terraform/block/TerraformSaplingBlock.java
@@ -1,7 +1,10 @@
 package com.terraformersmc.terraform.block;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
 import net.minecraft.block.SaplingBlock;
 import net.minecraft.block.sapling.SaplingGenerator;
+import net.minecraft.sound.BlockSoundGroup;
 
 /**
  * A simple wrapper around the parent SaplingBlock class allowing access to the constructor.
@@ -10,6 +13,10 @@ public class TerraformSaplingBlock extends SaplingBlock {
 
 	public TerraformSaplingBlock(SaplingGenerator generator, Settings settings) {
 		super(generator, settings);
+	}
+
+	public TerraformSaplingBlock(SaplingGenerator generator) {
+		this(generator, FabricBlockSettings.of(Material.PLANT).collidable(false).ticksRandomly().hardness(0).sounds(BlockSoundGroup.GRASS));
 	}
 
 }
