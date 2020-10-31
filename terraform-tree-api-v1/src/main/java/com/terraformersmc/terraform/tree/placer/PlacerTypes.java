dc857b81e532b44f81a3ac55b715cacebf07aada
coderbot <coderbot16@gmail.com>
2020-10-30 21:01:10 -0700

    Split off terraform-config-api-v1, delete fog hooks for now

--- terraform-tree-api-v1/src/main/java/com/terraformersmc/terraform/tree/placer/PlacerTypes.java
+++ terraform-tree-api-v1/src/main/java/com/terraformersmc/terraform/tree/placer/PlacerTypes.java
@@ -1,8 +1,9 @@
 package com.terraformersmc.terraform.tree.placer;
 
 import com.mojang.serialization.Codec;
-import com.terraformersmc.terraform.mixin.InvokerFoliagePlacerType;
-import com.terraformersmc.terraform.mixin.InvokerTrunkPlacerType;
+import com.terraformersmc.terraform.tree.mixin.InvokerFoliagePlacerType;
+import com.terraformersmc.terraform.tree.mixin.InvokerTrunkPlacerType;
+
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.foliage.FoliagePlacer;
 import net.minecraft.world.gen.foliage.FoliagePlacerType;
@@ -10,10 +11,14 @@ import net.minecraft.world.gen.trunk.TrunkPlacer;
 import net.minecraft.world.gen.trunk.TrunkPlacerType;
 
 public class PlacerTypes {
+	// Deprecated annotation is just a warning for non-internal references
+	@SuppressWarnings("deprecation")
 	public static <P extends FoliagePlacer> FoliagePlacerType<P> registerFoliagePlacer(String id, Codec<P> codec) {
 		return InvokerFoliagePlacerType.callRegister(id, codec);
 	}
 
+	// Deprecated annotation is just a warning for non-internal references
+	@SuppressWarnings("deprecation")
 	public static <P extends TrunkPlacer> TrunkPlacerType<P> registerTrunkPlacer(String id, Codec<P> codec) {
 		return InvokerTrunkPlacerType.callRegister(id, codec);
 	}
