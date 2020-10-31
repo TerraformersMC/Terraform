7ea1fa6d6989a68dcdd458037801ad0a63be359e
coderbot <coderbot16@gmail.com>
2020-10-31 00:37:54 -0700

    Fix sandy trees not being registered

--- terraform-tree-api-v1/src/main/java/com/terraformersmc/terraform/tree/feature/TerraformTreeFeatures.java
+++ terraform-tree-api-v1/src/main/java/com/terraformersmc/terraform/tree/feature/TerraformTreeFeatures.java
@@ -7,11 +7,7 @@ import net.minecraft.world.gen.feature.FeatureConfig;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 
 public class TerraformTreeFeatures {
-	public static Feature<TreeFeatureConfig> SANDY_TREE;
-
-	public static void init() {
-		SANDY_TREE = register("sandy_tree", new SandyTreeFeature(TreeFeatureConfig.CODEC));
-	}
+	public static Feature<TreeFeatureConfig> SANDY_TREE = register("sandy_tree", new SandyTreeFeature(TreeFeatureConfig.CODEC));
 
 	public static <T extends Feature<FC>, FC extends FeatureConfig> T register(String name, T feature) {
 		return Registry.register(Registry.FEATURE, new Identifier("terraform", name), feature);
