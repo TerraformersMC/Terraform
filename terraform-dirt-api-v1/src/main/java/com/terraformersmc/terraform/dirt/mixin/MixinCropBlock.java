2b2e6e590e58da630ecb39431fc8f5f65ac76934
Vaerian <vaeriann@gmail.com>
2020-12-02 17:51:05 -0700

    Fixed the crop block mixin

--- terraform-dirt-api-v1/src/main/java/com/terraformersmc/terraform/dirt/mixin/MixinCropBlock.java
+++ terraform-dirt-api-v1/src/main/java/com/terraformersmc/terraform/dirt/mixin/MixinCropBlock.java
@@ -9,8 +9,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CropBlock.class)
 public class MixinCropBlock {
-	@Redirect(method = "getAvailableMoisture(Lnet/minecraft/block/Block;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)F", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractBlock$AbstractBlockState;isOf(Lnet/minecraft/block/Block;)Z"))
-	private static boolean onGetAvailableMoisture(AbstractBlock.AbstractBlockState state, Block block) {
+	@Redirect(method = "Lnet/minecraft/block/CropBlock;getAvailableMoisture(Lnet/minecraft/block/Block;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)F", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
+	private static boolean onGetAvailableMoisture(BlockState state, Block block) {
 		return state.isOf(block) || (block == Blocks.FARMLAND && TerraformDirtBlockTags.FARMLAND.contains(state.getBlock()));
 	}
 }
