9e9ba2a539990cf36eaea01089b8b3d31eaebde4
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-04-10 20:32:28 -0400

    Fix wrong mixin descriptor

--- terraform-tree-api-v1/src/main/java/com/terraformersmc/terraform/tree/mixin/MixinTreeFeature.java
+++ terraform-tree-api-v1/src/main/java/com/terraformersmc/terraform/tree/mixin/MixinTreeFeature.java
@@ -26,7 +26,7 @@ public abstract class MixinTreeFeature<FC extends FeatureConfig> extends Feature
 	}
 
 	// TODO: Not sure if the redirect is better or worse here
-	@Redirect(method = "generate(Lnet/minecraft/world/gen/feature/util/FeatureContext;)Z", at = @At(value = "INVOKE", target = "net/minecraft/world/gen/feature/TreeFeature.canPlaceTreeOn(Lnet/minecraft/world/TestableWorld;Lnet/minecraft/util/math/BlockPos;)Z"))
+	@Redirect(method = "generate(Lnet/minecraft/world/StructureWorldAccess;Ljava/util/Random;Lnet/minecraft/util/math/BlockPos;Ljava/util/Set;Ljava/util/Set;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/world/gen/feature/TreeFeatureConfig;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/feature/TreeFeature;canPlaceTreeOn(Lnet/minecraft/world/TestableWorld;Lnet/minecraft/util/math/BlockPos;)Z"))
 	private boolean terrestria$allowSandyTreeGeneration(TestableWorld world, BlockPos pos) {
 		if ((this instanceof ExtendedTreeGeneration)) {
 			return ((ExtendedTreeGeneration) this).canGenerateOn(world, pos);
