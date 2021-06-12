cc217702822510f1d8511d57f7a644e20a2fdc24
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-06-12 15:07:16 -0400

    Fix mixin issues on 1.17

--- terraform-dirt-api-v1/src/main/java/com/terraformersmc/terraform/dirt/mixin/MixinTrunkPlacer.java
+++ terraform-dirt-api-v1/src/main/java/com/terraformersmc/terraform/dirt/mixin/MixinTrunkPlacer.java
@@ -4,6 +4,7 @@ import com.terraformersmc.terraform.dirt.DirtBlocks;
 import com.terraformersmc.terraform.dirt.TerraformDirtBlockTags;
 import com.terraformersmc.terraform.dirt.TerraformDirtRegistry;
 
+import java.util.Random;
 import java.util.function.BiConsumer;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,14 +16,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.ModifiableTestableWorld;
+import net.minecraft.world.TestableWorld;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.trunk.TrunkPlacer;
 
 @Mixin(TrunkPlacer.class)
 public class MixinTrunkPlacer {
 	@Inject(method = "setToDirt", at = @At("HEAD"), cancellable = true)
-	private static void notAlwaysDirt(ModifiableTestableWorld world, BiConsumer<BlockPos, BlockState> replacer, BlockPos pos, TreeFeatureConfig config, CallbackInfo ci) {
+	private static void notAlwaysDirt(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, BlockPos pos, TreeFeatureConfig config, CallbackInfo ci) {
 		if (!world.testBlockState(pos, state -> state.isIn(TerraformDirtBlockTags.SOIL))) {
 			return;
 		}
