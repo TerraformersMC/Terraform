c3b3076a509368e35ed28cc6984ebdc26d523366
MoriyaShiine <moriyashiine@gmail.com>
2021-07-29 20:54:52 -0500

    fix https://github.com/TerraformersMC/Terraform/issues/42

--- terraform-wood-api-v1/src/main/java/com/terraformersmc/terraform/sign/mixin/MixinSignEditScreen.java
+++ terraform-wood-api-v1/src/main/java/com/terraformersmc/terraform/sign/mixin/MixinSignEditScreen.java
@@ -4,21 +4,27 @@ import com.terraformersmc.terraform.sign.TerraformSign;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.client.gui.screen.ingame.SignEditScreen;
 import net.minecraft.client.render.TexturedRenderLayers;
-import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.util.SignType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(SignBlockEntityRenderer.class)
+@Mixin(SignEditScreen.class)
 @Environment(EnvType.CLIENT)
-public class MixinSignBlockEntityRenderer {
+public class MixinSignEditScreen {
+	@Shadow
+	@Final
+	private SignBlockEntity sign;
+	
 	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/TexturedRenderLayers;getSignTextureId(Lnet/minecraft/util/SignType;)Lnet/minecraft/client/util/SpriteIdentifier;"))
-	private SpriteIdentifier getSignTextureId(SignType signType, SignBlockEntity signBlockEntity) {
-		if (signBlockEntity.getCachedState().getBlock() instanceof TerraformSign) {
-			return new SpriteIdentifier(TexturedRenderLayers.SIGNS_ATLAS_TEXTURE, ((TerraformSign) signBlockEntity.getCachedState().getBlock()).getTexture());
+	private SpriteIdentifier getSignTextureId(SignType signType) {
+		if (sign.getCachedState().getBlock() instanceof TerraformSign) {
+			return new SpriteIdentifier(TexturedRenderLayers.SIGNS_ATLAS_TEXTURE, ((TerraformSign) sign.getCachedState().getBlock()).getTexture());
 		}
 		return TexturedRenderLayers.getSignTextureId(signType);
 	}
