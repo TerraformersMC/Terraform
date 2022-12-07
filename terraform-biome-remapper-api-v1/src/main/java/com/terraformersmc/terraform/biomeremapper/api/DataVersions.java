f66c44600db88a82c1e3dbc983a2aa5d0a20b51a
gniftygnome <minecraft@gnifty.net>
2022-12-07 10:56:45 -0800

    Add 1.19.2 and 1.19.3 data version constants for the biome remapper API.

--- terraform-biome-remapper-api-v1/src/main/java/com/terraformersmc/terraform/biomeremapper/api/DataVersions.java
+++ terraform-biome-remapper-api-v1/src/main/java/com/terraformersmc/terraform/biomeremapper/api/DataVersions.java
@@ -10,4 +10,6 @@ public final class DataVersions {
 	public static final int V_1_18_2 = 2970;
 	public static final int V_1_19   = 3097;
 	public static final int V_1_19_1 = 3107;
+	public static final int V_1_19_2 = 3118;
+	public static final int V_1_19_3 = 3211;
 }
