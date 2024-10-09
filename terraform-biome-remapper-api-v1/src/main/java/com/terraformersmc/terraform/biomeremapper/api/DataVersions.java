a7ac9deb69d747c40d8e1f70739a237570012b11
gniftygnome <minecraft@gnifty.net>
2024-10-09 13:20:48 -0700

    Add data version for 1.21.2.
    - Add data version for 1.21.2

--- terraform-biome-remapper-api-v1/src/main/java/com/terraformersmc/terraform/biomeremapper/api/DataVersions.java
+++ terraform-biome-remapper-api-v1/src/main/java/com/terraformersmc/terraform/biomeremapper/api/DataVersions.java
@@ -7,6 +7,7 @@ public final class DataVersions {
 		return;
 	}
 
+	public static final int V_1_21_2 = 4073;
 	public static final int V_1_21_1 = 3954;
 	public static final int V_1_21   = 3948;
 	public static final int V_1_20_6 = 3838;
