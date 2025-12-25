ba5ea45017aabd071897fee29f77b628a108782b
gniftygnome <minecraft@gnifty.net>
2025-12-24 20:21:51 -0800

    Update to 26.1 to the extent of getting successful builds.
    * Something is wrong with Loom config of the testmod(s).
    * The sapling trade helper is no more.
    * Mixins need work.

--- terraform-tree-api-v1/src/main/java/com/terraformersmc/terraform/tree/impl/merchant/TerraformSaplingTradeHelperImpl.java
+++ terraform-tree-api-v1/src/main/java/com/terraformersmc/terraform/tree/impl/merchant/TerraformSaplingTradeHelperImpl.java
@@ -1,27 +1,33 @@
 package com.terraformersmc.terraform.tree.impl.merchant;
 
-import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.npc.villager.VillagerTrades;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.item.trading.VillagerTrade;
+import net.minecraft.world.item.trading.VillagerTrades;
 import net.minecraft.world.level.ItemLike;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+import static net.minecraft.world.item.trading.VillagerTrades.resourceKey;
+
 public final class TerraformSaplingTradeHelperImpl {
+	public static final ResourceKey<VillagerTrade> WANDERING_TRADER_EMERALD_SPRUCE_SAPLING = resourceKey("wandering_trader/emerald_spruce_sapling");
+
 	@SuppressWarnings("UnnecessaryReturnStatement")
 	private TerraformSaplingTradeHelperImpl() {
 		return;
 	}
 
 	public static void registerWanderingTraderSaplingTrades(ItemLike... saplings) {
+/* TODO:  This feature is no longer useful, as trades are data.  Evaluate whether a datagen helper is useful.
 		TradeOfferHelper.registerWanderingTraderOffers(builder ->
 				builder.addOffersToPool(TradeOfferHelper.WanderingTraderOffersBuilder.SELL_COMMON_ITEMS_POOL,
 						Arrays.stream(saplings).map(SellSaplingFactory::new).collect(Collectors.toSet())));
@@ -38,5 +44,6 @@ public final class TerraformSaplingTradeHelperImpl {
 		public @Nullable MerchantOffer getOffer(ServerLevel world, Entity entity, RandomSource random) {
 			return new MerchantOffer(new ItemCost(Items.EMERALD, 5), this.sapling, 8, 1, 0.05f);
 		}
+*/
 	}
 }
