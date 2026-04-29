a52397939c2b48c9ec9b5d65835b4fcf32385bd0
gniftygnome <minecraft@gnifty.net>
2026-04-28 20:17:08 -0700

    DirtBlocks allows nulls and needs to be marked.
    - Mark DirtBlocks args as nullable

--- terraform-dirt-api-v1/src/main/java/com/terraformersmc/terraform/dirt/api/DirtBlocks.java
+++ terraform-dirt-api-v1/src/main/java/com/terraformersmc/terraform/dirt/api/DirtBlocks.java
@@ -5,16 +5,24 @@ import com.terraformersmc.terraform.dirt.api.block.TerraformFarmlandBlock;
 import com.terraformersmc.terraform.dirt.api.block.TerraformGrassBlock;
 import com.terraformersmc.terraform.dirt.api.block.TerraformSnowyBlock;
 import net.minecraft.world.level.block.Block;
+import org.jspecify.annotations.Nullable;
 
 /**
- * Stores a collection of DirtBlocks. These blocks must have already been registered to the block registry.
+ * Stores a collection of DirtBlocks.
+ * These blocks must already be registered to the block registry.
+ *
+ * @param dirtBlock mandatory Block of the base dirt
+ * @param grassBlock optional TerraformGrassBlock of the grassy dirt
+ * @param dirtPathBlock optional TerraformDirtPathBlock of trampled path
+ * @param podzolBlock  optional TerraformSnowyBlock of the podzol form
+ * @param farmBlock optional TerraformFarmlandBlock of the cultivated dirt
  */
 @SuppressWarnings("unused")
 public record DirtBlocks(
 	Block dirtBlock,
-	TerraformGrassBlock grassBlock,
-	TerraformDirtPathBlock dirtPathBlock,
-	TerraformSnowyBlock podzolBlock,
-	TerraformFarmlandBlock farmBlock
+	@Nullable TerraformGrassBlock grassBlock,
+	@Nullable TerraformDirtPathBlock dirtPathBlock,
+	@Nullable TerraformSnowyBlock podzolBlock,
+	@Nullable TerraformFarmlandBlock farmBlock
 ) {
 }
